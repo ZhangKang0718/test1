a2a5966614c83433a24f80749c8428966a7119da
ZhangKang0718 <1067462361@qq.com>
2025-09-08 18:37:28 +0800

    feat: commit test

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
 console.log('hello world')
+app.config.optionMergeStrategies. = (parent, child, vm) => {
+    return child
+}
+
+
+app.config.performance = app.config.performance || {};
+
+this._init = function (options) {
+    if (options && options.el) {
+        this.$mount(options.el);
+    }
+}
